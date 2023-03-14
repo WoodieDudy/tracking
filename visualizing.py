e87664cbc8f7cfb45fd2de58ac69ce88ebfbe602
WoodieDudy <goshagks@yandex.ru>
2023-03-14 12:46:26 +0500

    speed up plotting

--- visualizing.py
+++ visualizing.py
@@ -2,7 +2,7 @@ import os.path
 from typing import ClassVar
 
 import numpy as np
-from PIL import ImageFont, ImageDraw, Image
+import cv2
 
 
 class TrackVisualizer:
@@ -10,34 +10,35 @@ class TrackVisualizer:
         self._border_size = 6
         self._border_color = (100, 178, 25)
         self._text_color = (45, 200, 45)
-        self._clothes_violation_border_color = (255, 0, 0)
-        self._clothes_default_border_color = (0, 255, 0)
-        self.font = ImageFont.truetype("assets/arial.ttf", 20)
 
-    def draw_box(self, draw, coords, border_color=None):
+    def draw_box(self, img, coords, border_color=None):
         border_color = border_color or self._border_color
 
         x1, y1, x2, y2 = coords
-        draw.rectangle((x1, y1, x2, y2), outline=border_color, width=self._border_size)
+        cv2.rectangle(img, (x1, y1), (x2, y2), border_color, self._border_size)
 
-    def draw_label(self, draw, coords, label, font, down=True):
+    def draw_label(self, img, coords, label, down=True):
         x1, y1, x2, y2 = coords
-        text_bg_w, text_bg_h = draw.textsize(label, font)
+        FONT = cv2.FONT_HERSHEY_SIMPLEX
+        FONT_SCALE = 1.0
+        FONT_THICKNESS = 2
+        bg_color = (255, 255, 255)
+        label_color = (0, 0, 0)
+
+        (text_bg_w, text_bg_h), baseline = cv2.getTextSize(label, FONT, FONT_SCALE, FONT_THICKNESS)
+
         if down:
             text_x1, text_y1 = x2, y2 - text_bg_h
         else:
             text_x1, text_y1 = x2, y1
 
-        draw.rectangle((text_x1, text_y1, text_x1 + text_bg_w, text_y1 + text_bg_h), fill='black')
-        draw.text((text_x1, text_y1), label, font=font, fill=self._text_color)
+        cv2.rectangle(img, (x1, y1), (x2, y2), bg_color, -1, self._border_size)
+
+        cv2.putText(img, label, (text_x1, text_y1), FONT, FONT_SCALE, label_color, 1, 2)
 
     def plot_tracking(self, image_rgb, bbs):
-        pil_image = Image.fromarray(image_rgb)
-        draw = ImageDraw.Draw(pil_image)
-
         for i, bb in enumerate(bbs):
-            self.draw_box(draw, bb.xyxy, border_color=(255, 0, 255))
-            self.draw_label(draw, bb.xyxy, bb.label, self.font, down=False)
+            self.draw_box(image_rgb, bb.xyxy, border_color=(255, 0, 255))
+            self.draw_label(image_rgb, bb.xyxy, bb.label, down=False)
 
-        numpy_image = np.array(pil_image)  # type: ignore
-        return numpy_image
+        return image_rgb
